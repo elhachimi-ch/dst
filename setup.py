04ecf743cf48b548828d7c9d72361bfa953d21b7
elhachimi-ch <elhachimi.ch@gmail.com>
2026-06-01 03:53:47 +0800

    Remove specific version constraint for numpy in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     
 setuptools.setup(
     name="data-science-toolkit",
-    version="0.1.067",
+    version="0.1.068",
     author="EL HACHIMI CHOUAIB",
     author_email="elhachimi.ch@gmail.com",
     description="Data Science Toolkit (DST) is a Python library that helps implement data science related project with ease.",
@@ -28,7 +28,7 @@ setuptools.setup(
         "setuptools>=42",
         "wheel",
         "pandas",
-        "numpy==1.25",
+        "numpy",
         "tensorflow",
         "scikit-learn",
         "seaborn",
